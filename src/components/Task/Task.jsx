7c2f259a006bf3726f688e5230d0ce1de4e1d5ed
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-02-05 00:11:08 +0200

    task-1_1

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,15 +1,25 @@
 import css from './Task.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteTask, toggleCompleted } from '../../redux/actions';
 
 export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  const hendleToggle = () => dispatch(toggleCompleted(task.id));
+
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
         checked={task.completed}
+        onChange={hendleToggle}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>close</button>
+      <button className={css.btn} onClick={handleDelete}>
+        close
+      </button>
     </div>
   );
 };
