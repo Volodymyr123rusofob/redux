51a44974a468b404e034f28942db30fb66d7d98a
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-02-05 23:58:09 +0200

    task-1-1

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -1,4 +1,11 @@
 import { statusFilters } from './constans';
+import {
+  addTask,
+  deleteTask,
+  setStatusFilter,
+  toggleCompleted,
+} from './actions';
+import { createReducer } from '@reduxjs/toolkit';
 
 const tasksInitialState = [
   { id: 0, text: 'Learn HTML and CSS', completed: true },
@@ -8,46 +15,33 @@ const tasksInitialState = [
   { id: 4, text: 'Build amazing apps', completed: false },
 ];
 
-const tasksReducer = (state = tasksInitialState, action) => {
-  switch (action.type) {
-    case 'tasks/addTask':
+export const tasksReducer = createReducer(tasksInitialState, builder => {
+  builder
+    .addCase(addTask, (state, action) => {
       return [...state, action.payload];
-    case 'tasks/deleteTask':
+    })
+    .addCase(deleteTask, (state, action) => {
       return state.filter(task => task.id !== action.payload);
-    case 'tasks/toggleCompleted':
+    })
+    .addCase(toggleCompleted, (state, action) => {
       return state.map(task => {
         if (task.id !== action.payload) {
           return task;
         }
         return { ...task, completed: !task.completed };
       });
-    default:
-      return state;
-  }
-};
+    });
+});
 
 const filtersInitialState = {
   status: statusFilters.all,
 };
 
-const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case 'filters/setStatusFilter':
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      };
-    default:
-      return state;
-  }
-};
-
-export const rootReducer = (state = {}, action) => {
-  return {
-    tasks: tasksReducer(state.tasks, action),
-    filters: filtersReducer(state.filters, action),
-  };
-};
+export const filtersReducer = createReducer(filtersInitialState, builder => {
+  builder.addCase(setStatusFilter, (state, action) => {
+    return {
+      ...state,
+      status: action.payload,
+    };
+  });
+});
